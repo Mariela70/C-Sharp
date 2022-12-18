d9c64b6d0e81635515966435e0efe2040b75ec3e
Mariela <marielamiteva97@abv.bg>
2022-12-18 18:59:32 +0200

    WarCraft

---
+++ O-O-P/Class/WarCraft/Entities/Characters/Warrior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Entities.Characters.Contracts;
+
+namespace WarCroft.Entities.Characters
+{
+    public class Warrior : IAttacker
+    {
+        public Warrior(string name)
+        {
+        }
+
+        public void Attack(Character character)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
