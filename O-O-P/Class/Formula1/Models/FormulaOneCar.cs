6b5ac11f1d9f7b6bb32c8639c668891d0dd4a5a0
Mariela <marielamiteva97@abv.bg>
2022-12-19 17:40:47 +0200

    Formula1

---
+++ O-O-P/Class/Formula1/Models/FormulaOneCar.cs
@@ -0,0 +1,74 @@
+﻿using Formula1.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        private string model;
+        private int horsepower;
+        private double engineDisplacement;
+
+        protected FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            this.Model = model;
+            this.Horsepower = horsepower;
+            this.EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model 
+        {
+            get => model;
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"Invalid car model: { model }.");
+                }
+                
+                
+            
+                model = value;
+            }
+        }
+
+        public int Horsepower 
+        {
+            get => horsepower;
+            private set 
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException($"Invalid car horsepower: { horsepower }.");
+                }
+            
+                horsepower = value; 
+            }
+        }
+
+        public double EngineDisplacement 
+        {
+            get => engineDisplacement;
+            private set
+            {
+                if (value < 1.6 || value > 2.00)
+                {
+                    throw new ArgumentException($"Invalid car engine displacement: { engineDisplacement }.");
+                }
+
+                engineDisplacement = value;
+            }
+
+
+        }
+
+
+        public double RaceScoreCalculator(int laps)
+        {
+           return engineDisplacement / horsepower * laps;
+
+        }
+    }
+}
---
+++ O-O-P/Class/Formula1/Models/Race.cs
@@ -0,0 +1,67 @@
+﻿using Formula1.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+        private readonly ICollection<IPilot> pilots;
+
+        public Race(string raceName, int numberOfLaps)
+        {
+            this.raceName = raceName;
+            this.numberOfLaps = numberOfLaps;
+        }
+
+        public string RaceName 
+        {
+            get => raceName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"Invalid pilot name: { raceName }.");
+                }
+
+                raceName = value;
+            }
+
+        }
+
+        public int NumberOfLaps 
+        {
+            get => numberOfLaps; 
+            private set 
+            {
+                if (value<1)
+                {
+                    throw new ArgumentException($"Invalid lap numbers: { numberOfLaps}.");
+                }
+                numberOfLaps = value; 
+            }
+        }
+
+        public bool TookPlace { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICollection<IPilot> Pilots => throw new NotImplementedException();
+
+        public void AddPilot(IPilot pilot)
+        {
+            pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+           StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The { raceName } race has:");
+            sb.AppendLine($"Participants: {string.Join(" ,", pilots)}");
+            sb.AppendLine($"Number of laps: { numberOfLaps}");
+            sb.AppendLine($"Took place: { TookPlace }");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
