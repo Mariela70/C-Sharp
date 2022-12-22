5b71a3b40f3d47c2e089e8d77e9c1e74b4f38396
Mariela <marielamiteva97@abv.bg>
2022-12-22 21:01:30 +0200

    Ski Rental

---
+++ Ski Rental/SkiRental.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        List<Ski> data;
+
+        public SkiRental(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Ski>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+
+        public void Add(Ski ski) 
+        {
+            if (Count<Capacity)
+            {
+                data.Add(ski);
+            }
+           
+        
+        }
+        public bool Remove(string manufacturer, string model) 
+        {
+            Ski ski = data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+            if (ski == null) 
+            {
+             return false;
+            }
+            return data.Remove(ski);
+
+        }
+        public Ski GetNewestSki() 
+        {
+         return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        
+        }
+        public Ski GetSki(string manufacturer, string model) 
+        {
+         return data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+        
+        }
+        public string GetStatistics() 
+        {
+         StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {Name}");
+
+            foreach (Ski ski in data) 
+            {
+                sb.AppendLine(ski.ToString());
+            
+            }
+            return sb.ToString().TrimEnd();
+
+        }
+
+    }
+}
